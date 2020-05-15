576c4f68ace7cb670147b8e36293bbc363fd7c91
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-05-15 07:14:46 -0700

    ring buffer tests are passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,47 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.storage = []
+        self.capacity = capacity
+        self.counter = 0
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            # print('storage: ', self.storage)
+            # print('counter : ',self.counter)
+            if self.counter == self.capacity:
+                self.counter = 0
+                self.storage[self.counter] = item
+                self.counter += 1
+            else:
+                self.storage[self.counter] = item
+                self.counter += 1
+
+        else:
+            self.storage.append(item)
+            self.counter += 1
+
 
     def get(self):
-        pass
+        return self.storage
+
+# ring = RingBuffer(3)
+# ring.append('a')
+# ring.append('b')
+# ring.append('c')
+
+
+# print(ring.get())
+
+# ring.append('d')
+
+# print(ring.get())
+
+# ring.append('e')
+# ring.append('f')
+
+# print(ring.get())
+
+
+
+
+# ring.append('d')
